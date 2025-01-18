7822d79a9e33b3a07ebac4be4ea84c8308fa534e
MichalKalinowskii <kalinowskimichal638@gmail.com>
2025-01-18 13:13:23 +0100

    reactive way

--- Pociagi/Program.cs
+++ Pociagi/Program.cs
@@ -1,7 +1,11 @@
 ﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Xml.Linq;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace Pociagi
 {
@@ -9,72 +13,91 @@ namespace Pociagi
     {
         static async Task Main(string[] args)
         {
+            var trainScheduleObservable = CreateTrainScheduleObservable();
+
+            do { 
+                await trainScheduleObservable
+                    .Do(schedule => Console.WriteLine($"Pociąg: {schedule.TrainName}, Czas: {schedule.Time}, Kierunek: {schedule.Direction}, Rozkład: {schedule.Schedule}"))
+                    .ToList()
+                    .ToTask();
+                Console.WriteLine("\n#########################################\n");
+                Thread.Sleep(30000);
+
+            } while (true);
+        }
+
+        private static IObservable<TrainSchedule> CreateTrainScheduleObservable()
+        {
             var date = DateTime.Now.ToString("dd.MM.yy");
             string url = $"https://rozklad-pkp.pl/pl/sq?maxJourneys=10&start=yes&dirInput=&GUIREQProduct_0=on&GUIREQProduct_1=on&GUIREQProduct_2=on&GUIREQProduct_3=on&advancedProductMode=&boardType=dep&input=&input=Bydgoszcz+Politechnika&date={date}&dateStart=18.01.25&REQ0JourneyDate=18.01.25&time=23%3A59";
-            var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(url);
 
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
+            return Observable.Using(
+                () => new HttpClient(),
+                client => Observable.FromAsync(() => client.GetStringAsync(url))
+                    .Select(html =>
+                    {
+                        var htmlDocument = new HtmlDocument();
+                        htmlDocument.LoadHtml(html);
+                        return htmlDocument;
+                    })
+                    .SelectMany(doc =>
+                    {
+                        var oddRows = doc.DocumentNode.SelectNodes("//tr[@class='odd']");
+                        var evenRows = doc.DocumentNode.SelectNodes("//tr[@class='even']");
 
-            List<TrainSchedule> schedules = new List<TrainSchedule>();
+                        var oddSchedules = DecodeRows(oddRows);
+                        var evenSchedules = DecodeRows(evenRows);
 
-            var oddsRows = htmlDocument.DocumentNode.SelectNodes("//tr[@class='odd']");
-            var evenRows = htmlDocument.DocumentNode.SelectNodes("//tr[@class='even']");
-
-            schedules.AddRange(DecodeRows(oddsRows));
-            schedules.AddRange(DecodeRows(evenRows));
+                        return oddSchedules.Concat(evenSchedules);
+                    })
+            );
         }
 
-
-        public static List<TrainSchedule> DecodeRows(HtmlNodeCollection rows)
+        private static IEnumerable<TrainSchedule> DecodeRows(HtmlNodeCollection rows)
         {
-            if (rows != null)
+            if (rows == null) yield break;
+
+            foreach (var row in rows)
             {
-                List<TrainSchedule> schedules = new();
+                var trainNameNode = row.SelectSingleNode(".//span[@class='train-name']");
+                var timeNode = row.SelectSingleNode(".//span[@class='time']");
+                var scheduleNodes = row.SelectNodes(".//a[starts-with(@href, '/pl/sq?')]");
 
-                foreach (var row in rows)
+                if (trainNameNode == null || timeNode == null || scheduleNodes == null) continue;
+
+                TimeOnly.TryParse(timeNode.InnerText.Trim(), out var time);
+
+                if (time < TimeOnly.FromDateTime(DateTime.Now)) continue;
+
+                var schedule = new TrainSchedule
                 {
-                    var trainNameNode = row.SelectSingleNode(".//span[@class='train-name']");
-                    var schedule = new TrainSchedule
+                    TrainName = trainNameNode.InnerText.Trim(),
+                    Time = time.ToString("HH:mm")
+                };
+
+                var scheduleString = string.Empty;
+                foreach (var node in scheduleNodes)
+                {
+                    var decodedHtml = WebUtility.HtmlDecode(node.InnerHtml).Replace("\n", string.Empty);
+
+                    if (schedule.Direction == null)
                     {
-                        TrainName = trainNameNode.InnerText.Trim(),
-                    };
-
-                    var time = row.SelectSingleNode(".//span[@class='time']");
-                    schedule.Time = time.InnerText.Trim();
-
-                    var scheduleNode = row.SelectNodes(".//a[starts-with(@href, '/pl/sq?')]");
-                    string scheduleString = string.Empty;
-
-                    foreach (var node in scheduleNode)
-                    {
-                        var decodedHtml = WebUtility.HtmlDecode(node.InnerHtml).Replace("\n", string.Empty);
-                        if (schedule.Direction is null)
-                        {
-                            schedule.Direction = decodedHtml;
-                            continue;
-                        }
-
-                        if (scheduleNode.Last().Equals(node))
-                        {
-                            scheduleString += decodedHtml;
-                            continue;
-                        }
-
-                        scheduleString += decodedHtml + " -> ";
+                        schedule.Direction = decodedHtml;
+                        continue;
                     }
 
-                    schedule.Schedule = scheduleString;
+                    if (scheduleNodes.Last().Equals(node))
+                    {
+                        scheduleString += decodedHtml;
+                        continue;
+                    }
 
-                    schedules.Add(schedule);
+                    scheduleString += decodedHtml + " -> ";
                 }
 
-                return schedules;
+                schedule.Schedule = scheduleString;
+                yield return schedule;
             }
-
-            return new List<TrainSchedule>();
         }
     }
-
 }
